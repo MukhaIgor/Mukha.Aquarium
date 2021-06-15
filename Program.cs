2666d026547332e0e34f25e6212c062f0b7bde80
Ігор Муха <mukha_i@donnu.edu.ua>
2021-06-15 17:54:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mukha.Aquarium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+}
